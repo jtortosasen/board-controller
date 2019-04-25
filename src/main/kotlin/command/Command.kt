748297815bc22356a60daeebeab44504c052e8e2
Wasdf <jorge.tortosa@outlook.com>
2019-04-25 12:05:19 +0200

    Support parameters to openled command

--- src/main/kotlin/command/Command.kt
+++ src/main/kotlin/command/Command.kt
@@ -3,6 +3,7 @@ package command
 import com.fazecast.jSerialComm.SerialPort
 
 sealed class Command {
+
     sealed class IO(val baudRate: Int = 0, val dataBits: Int = 0, val parity: Int = 0, val stopBits: Int = 0) : Command() {
         class OpenSlave9600B8N1 : IO(
             baudRate = 9600,
@@ -10,24 +11,20 @@ sealed class Command {
             parity = SerialPort.NO_PARITY,
             stopBits = SerialPort.ONE_STOP_BIT
         )
-
         class OpenSlave19200B8N1 : IO(
             baudRate = 19200,
             dataBits = 8,
             parity = SerialPort.NO_PARITY,
             stopBits = SerialPort.ONE_STOP_BIT
         )
-
         class OpenSlave19200B9N1 : IO(
             baudRate = 19200,
             dataBits = 8,
             parity = SerialPort.NO_PARITY,
             stopBits = SerialPort.ONE_STOP_BIT
         )
-
         class CloseSlave : IO()
         class SendSlave(val content: ByteArray) : IO()
-
         sealed class MasterMode: IO(){
             class Cirsa : MasterMode()
             class Franco: MasterMode()
@@ -35,7 +32,8 @@ sealed class Command {
         }
     }
 
-    class OpenLed : Command()
+    class OpenLed(val color: ByteArray) : Command()
+    class CloseLed: Command()
     class UpdateVideo : Command()
     class PlayVideo : Command()
     class Update : Command()
@@ -44,6 +42,7 @@ sealed class Command {
     class IdMacNACK : Command()
     class None : Command()
 
+
     companion object {
         fun get(command: Byte, content: ByteArray = byteArrayOf(0)): Command {
             return when (command) {
@@ -51,9 +50,10 @@ sealed class Command {
                 0x23.toByte() -> IO.OpenSlave19200B8N1()
                 0x24.toByte() -> IO.OpenSlave19200B9N1()
                 0x25.toByte() -> IO.CloseSlave()
-                0x45.toByte() -> IO.SendSlave(content)
+                0x45.toByte() -> IO.SendSlave(content = content)
                 0x93.toByte() -> IO.MasterMode.Sas()
-                0x30.toByte() -> OpenLed()
+                0x30.toByte() -> OpenLed(color = content)
+                0x31.toByte() -> CloseLed()
                 0xF0.toByte() -> UpdateVideo()
                 0xD0.toByte() -> PlayVideo()
                 0xE0.toByte() -> Update()
